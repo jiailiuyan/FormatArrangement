fc3648beb821561356b0c03814a55f15b0c91a2e
迹 <366006820@qq.com>
2014-12-17 10:22:38 +0800

    添加文档整理项目

---
+++ FormatArrangement/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace 迹I柳燕.FormatArrangement
+{
+    static class GuidList
+    {
+        public const string guidFormatArrangementPkgString = "6273ccde-7c8b-4f45-b234-6518819dc0fd";
+        public const string guidFormatArrangementCmdSetString = "04d14637-e8d0-48e5-a6c3-3029e352908e";
+
+        public static readonly Guid guidFormatArrangementCmdSet = new Guid(guidFormatArrangementCmdSetString);
+    };
+}
---
+++ FormatArrangement/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace 迹I柳燕.FormatArrangement
+{
+    static class PkgCmdIDList
+    {
+        public const uint DocumentationCommand =        0x100;
+
+
+    };
+}
